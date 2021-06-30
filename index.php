3b4ba2b04e77ecac3349a8c610a656c0a1de78da
Dara Audia <86711239+daraaudia@users.noreply.github.com>
2021-06-30 15:35:26 +0700

    Membuat codingan index.php

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>KAZUMA BARBERSHOP</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Patrick+Hand&family=Yeon+Sung&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@200&display=swap" rel="stylesheet">
+</head>
+    <nav>
+        <div class="logo">
+            <h4>SKINCARE N MAKE UP STORE</h4>
+        </div>
+
+        <ul>
+            <li><a href="index.php">HOME</a></li>
+            <li><a href="product.php">PRODUCT</a></li>
+            <li><a href="about.php">BRANCH</a></li>
+            <li><a href="contact.php">CONTACT US</a></li>
+            <li><a href="login.php">ADMIN</a></li>
+        </ul>
+    </nav>
+<body>
+    <center>
+        <h1>Welcome To Kazuma Barbershop</h1>
+    </center>
+
+    <div id="model">
+        <img class="model "src="img/cut1.jpg">
+        <img class="model"src="img/cut2.jpg">
+        <img class="model"src="img/cut3.jpg">
+        <img class="model"src="img/cut4.jpg">
+        <img class="model"src="img/cut5.jpg">
+        <img class="model"src="img/cut3.jpg">
+        <img class="model"src="img/cut6.jpg">
+        <img class="model"src="img/cut2.jpg">
+        <img class="model"src="img/cut7.jpg">
+        <img class="model"src="img/cut3.jpg">
+    </div>
+</body>
+</html>

cef7839b4263da8ee00bf947ca8c0c992d32fa66
Dara Audia <86711239+daraaudia@users.noreply.github.com>
2021-06-30 15:50:24 +0700

    Menambah codingan folder css

--- lihat.php
+++ lihat.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>KRITIK SARAN</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat&family=Pacifico&display=swap">
     <style>
         a {

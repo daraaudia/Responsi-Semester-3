99be451661aef9b1c5e60ace25f1ca96359213b3
Dara Audia <86711239+daraaudia@users.noreply.github.com>
2021-06-30 15:36:48 +0700

    Membuat file about.php

---
+++ about.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>KAZUMA BARBERSHOP</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Patrick+Hand&family=Yeon+Sung&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300&display=swap" rel="stylesheet">
+    <style>
+        h1 {
+            color: black;
+            font-family: 'Roboto Mono', monospace;
+        }
+    </style>
+</head>
+<body>
+    <nav>
+        <div class="logo">
+            <h4>SKINCARE N MAKE UP STORE</h4>
+        </div>
+
+        <ul>
+            <li><a href="index.php">HOME</a></li>
+            <li><a href="product.php">PRODUCT</a></li>
+            <li><a href="about.php">BRANCH</a></li>
+            <li><a href="contact.php">CONTACT US</a></li>
+            <li><a href="login.php">ADMIN</a></li>
+        </ul>
+    </nav>
+    <br><br>
+    <center>
+        <h1>KAZUMA BARBERSHOP BRANCH</h1>
+    </center>
+    <div id="model1">
+        <img class="model2 "src="img/barber1.jpg">
+        <br>
+        <center>
+            <h2>CABANG A</h2><br>
+            <h3>Jl. Pondok Indah</h3>
+            <h3>081208120812</h3>
+            <h3>10AM - 9PM</h3>
+        </center>
+    </div>
+    <div id="model2">
+        <img class="model2 "src="img/barber2.jpg">
+        <center>
+            <h2>CABANG B</h2><br>
+            <h3>Jl. Rasuna Said</h3>
+            <h3>082308230823</h3>
+            <h3>11AM - 9.30PM</h3>
+        </center>
+    </div>
+    <div id="model3">
+        <img class="model2"src="img/barber3.jpg">
+        <center>
+            <h2>CABANG C</h2><br>
+            <h3>Jl. Malioboro</h3>
+            <h3>085208520852</h3>
+            <h3>10AM - 9PM</h3>
+        </center>
+    </div>
+</body>
+</html>
